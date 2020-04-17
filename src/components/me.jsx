0ec4e767054744317caedc67dd01badc31d82aae
jackmorrison <jsm1317@ic.ac.uk>
2020-04-17 23:07:57 +0100

    Adds picture to 'me' page

--- src/components/me.jsx
+++ src/components/me.jsx
@@ -1,9 +1,29 @@
 import React, { Fragment } from "react";
 
+import avatar from "../assets/images/avatar.jpg";
+
+import styled from "styled-components";
+
 export default function Me() {
     return(
         <Fragment>
-        <h1>Me</h1>
+            <div class="me-outer">
+                <div class="me-left">
+                    <Image src={avatar} alt="Avatar" />
+                    <br />
+                    {/* Name: Jack Morrison
+                    Age:  */}
+                </div>
+                <div class="me-right">
+                    {/* Stats */}
+                </div>
+            </div>
+
         </Fragment>
     );
 }
+
+const Image = styled.img`
+    border-radius: 50%;
+    width: 50%;
+`;
