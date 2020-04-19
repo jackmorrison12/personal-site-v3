6b66eb6ca4954f93097d7838fe605564febc4e00
jackmorrison <jsm1317@ic.ac.uk>
2020-04-20 00:52:08 +0100

    Adds project info

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,21 +2,39 @@ import React, { Fragment } from "react";
 
 import projects from "../data/projects.json";
 
+import Fade from 'react-reveal/Fade';
+
 export default function Projects() {
     return(
         <Fragment>
-        <div class="sidenav">
+            <div class="sidenav">
 
-            {
-                projects.map((project) => {
-                    return(
-                        <a href="#">{project.name}</a>
-                    );
-                })
-            }
+                {
+                    projects.map((project) => {
+                        return(
+                            <div>
+                                <a href="#">{project.name}</a>
+                                <div class="box-badge-matrix">
+                                    <div class="box-badge-wrapper">
+                                        <div class="box-badge">
+                                            {project.type}
+                                        </div>
+                                    </div>
+                                </div>
+                                {
+                                    project.sources ? project.sources.map((source) => {
+                                        return(
+                                            <a href={source.url} class="proj-icon"><i class={`fab fa-${source.name} fa-3x`}></i></a>
+                                        );
+                                    }) : ""
+                                }
+                                
+                            </div>
+                        );
+                    })
+                }
 
-        </div>
-
+            </div>
         <div class="content">
         </div>
         </Fragment>
