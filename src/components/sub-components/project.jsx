1f438109360af4caffc7c7e45833f204937e53d5
jackmorrison <jsm1317@ic.ac.uk>
2020-04-21 21:26:13 +0100

    Projects expand to show more detail when clicked

--- src/components/sub-components/project.jsx
+++ src/components/sub-components/project.jsx
@@ -1,16 +1,27 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, Component, useState } from "react";
 
 import Fade from 'react-reveal/Fade';
 
 class Project extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          projOpen: false
+        };
+      }
+
     render() {
         return(
             <Fragment>    
-                <div class="project">
+                <div class="project" onClick={() => this.setState({projOpen : !this.state.projOpen})}>
                     <h1>{this.props.data.name}</h1>
                     <h2>{this.props.data.type}</h2>
                     <h3>{this.props.data.dates}</h3>
-                    <p>{this.props.data.long_desc}</p>
+                    <p>{this.props.data.short_desc}</p>
+                    <div className={this.state.projOpen ? "project-expanded" : "project-collapsed"} >
+                        <p>{this.props.data.long_desc}</p>
+                    </div>
                 </div>         
             </Fragment>
         );
