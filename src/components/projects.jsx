cfb6770db0f0ee07be542da3ab9a978b43f21ad4
jackmorrison <jsm1317@ic.ac.uk>
2020-04-21 03:17:17 +0100

    Adds dark and light mode

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,36 +7,7 @@ import Fade from 'react-reveal/Fade';
 export default function Projects() {
     return(
         <Fragment>
-            {/* <div class="sidenav">
-
-                {
-                    projects.map((project) => {
-                        return(
-                            <div>
-                                <a href="#">{project.name}</a>
-                                <div class="box-badge-matrix">
-                                    <div class="box-badge-wrapper">
-                                        <div class="box-badge">
-                                            {project.type}
-                                        </div>
-                                    </div>
-                                </div>
-                                {
-                                    project.sources ? project.sources.map((source) => {
-                                        return(
-                                            <a href={source.url} class="proj-icon"><i class={`fab fa-${source.name} fa-3x`}></i></a>
-                                        );
-                                    }) : ""
-                                }
-                                
-                            </div>
-                        );
-                    })
-                }
-
-            </div>
-        <div class="content">
-        </div> */}
+            
         </Fragment>
     );
 }
