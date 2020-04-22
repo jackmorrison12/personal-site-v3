8dfb668fa3bcbe8694f63df5fa17a5be7ea1fe83
jackmorrison <jsm1317@ic.ac.uk>
2020-04-22 03:45:42 +0100

    Adds tech stack to projects

--- src/components/sub-components/project.jsx
+++ src/components/sub-components/project.jsx
@@ -14,13 +14,29 @@ class Project extends Component {
     render() {
         return(
             <Fragment>    
-                <div class="project" onClick={() => this.setState({projOpen : !this.state.projOpen})}>
-                    <h1>{this.props.data.name}</h1>
-                    <h2>{this.props.data.type}</h2>
-                    <h3>{this.props.data.dates}</h3>
-                    <p>{this.props.data.short_desc}</p>
-                    <div className={this.state.projOpen ? "project-expanded" : "project-collapsed"} >
-                        <p>{this.props.data.long_desc}</p>
+                <div class= {"project " + (this.state.projOpen ? "project-large" : "project-small")} onClick={() => this.setState({projOpen : !this.state.projOpen})}>
+                    <div className="project-content">
+                        <h1>{this.props.data.name}</h1>
+                        <div class="box-badge-matrix">
+                        { this.props.data.tech ?
+                                            this.props.data.tech.map(tech => {
+                                                return (
+                                                    <div class="box-badge-wrapper">
+                                                        <div class="box-badge">
+                                                            {tech}
+                                                        </div>
+                                                    </div>
+                                                );
+                                            }) 
+                                            : ""
+                                        }
+                        </div>
+                        <h2>{this.props.data.type}</h2>
+                        <h3>{this.props.data.dates}</h3>
+                        <p>{this.props.data.short_desc}</p>
+                        <div className={this.state.projOpen ? "project-expanded" : "project-collapsed"} >
+                            <p>{this.props.data.long_desc}</p>
+                        </div>
                     </div>
                 </div>         
             </Fragment>
