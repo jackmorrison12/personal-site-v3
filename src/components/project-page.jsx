a6e0e645515b60241f4f8dafda1879878a686cc8
jackmorrison <jsm1317@ic.ac.uk>
2020-04-25 00:39:03 +0100

    Adds pictures and images to project pages

--- src/components/project-page.jsx
+++ src/components/project-page.jsx
@@ -1,11 +1,61 @@
 import React, { Fragment, Component } from "react";
+import YouTube from 'react-youtube';
+
 
 class ProjectPage extends Component  {
 
     render () {
         return(
             <Fragment>
-                <h1>{this.props.data.name}</h1>
+                <div className="project-page-content">
+                    <div className="project-badge-matrix">
+                        { this.props.data.tech ?
+                            this.props.data.tech.map(tech => {
+                                return (
+                                    <div className="box-badge-wrapper">
+                                        <div className="box-badge">
+                                            {tech}
+                                        </div>
+                                    </div>
+                                );
+                            }) 
+                            : ""
+                        }
+                    </div>
+                    <h1 className="project-header">{this.props.data.name}</h1>
+                    <h2 className="project-subheader">{this.props.data.dates}</h2>
+                    <p className="project-short-desc">{this.props.data.short_desc}</p>     
+                    <div className="project-page-layout">
+                        <p className="project-page-long-desc">                     
+                            {
+                                this.props.data.long_desc.split('\n').map((item, i) => {
+                                    return <p key={1}>{item}</p>;
+                                })
+                            }
+                        </p>
+                        <div className="project-page-image-container">
+                            {
+                                this.props.data.pictures ?
+                                    this.props.data.pictures.map((pic, i) => {
+                                        return (                            
+                                            <img className="project-page-image" src={require(`../assets/images/projects/${pic}`)} alt={this.props.data.name + " Image " + i}/>
+                                        );
+                                    })
+                                : ""
+                            }
+                            {
+                                this.props.data.videos ?
+                                this.props.data.videos.map((vid, i) => {
+                                    return (                            
+                                        <YouTube className="project-page-video"  videoId={vid.id}  />
+                                    );
+                                })
+                            : ""  
+                            }
+                        </div>
+                    </div>           
+                </div>
+
             </Fragment>
         );
     }
