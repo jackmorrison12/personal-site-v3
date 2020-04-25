cba4873719d94282a9f83c27416f7c32be0235b2
jackmorrison <jsm1317@ic.ac.uk>
2020-04-25 01:50:39 +0100

    Adds construction logo to CV page and reorders projects

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -5,11 +5,8 @@ import cv from "../assets/files/jack-cv.pdf"
 export default function CV() {
     return(
         <Fragment>
-            <div style={{  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  flexDirection: "column",
-  height: "100vh"}}>
+            <div style={{  display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "100vh"}}>
+                <i class="fas fa-tools" style={{fontSize:"60px"}}></i>
                 <h1 style={{textAlign:"center", padding:"20px"}}>This page is currently under construction. <br />You can download a copy of my CV below:</h1>
                 <a href = {cv} target = "_blank" style={{border:"none"}}>
                     <button className="projects-button">
