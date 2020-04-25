2dc403853d4ee8e35833930aedc1bcb66610265c
jackmorrison <jsm1317@ic.ac.uk>
2020-04-25 23:05:33 +0100

    Moves epq and cv to public folder so they can have static URLs

--- src/components/cv.jsx
+++ src/components/cv.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 
-import cv from "../assets/files/jack-cv.pdf"
+// import cv from "../assets/files/jack-cv.pdf"
 
 export default function CV() {
     return(
@@ -8,7 +8,7 @@ export default function CV() {
             <div style={{  display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "calc(100vh - 20px)"}}>
                 <i class="fas fa-tools" style={{fontSize:"60px"}}></i>
                 <h1 style={{textAlign:"center", padding:"20px"}}>This page is currently under construction. <br />You can download a copy of my CV below:</h1>
-                <a href = {cv} target = "_blank" style={{border:"none"}}>
+                <a href = "/files/jack-cv.pdf" target = "_blank" style={{border:"none"}}>
                     <button className="projects-button">
                         Download my CV
                     </button>
