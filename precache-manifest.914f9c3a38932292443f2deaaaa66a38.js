0cb20aa378ab81c6b857fea54298169ad5358689
jackmorrison12 <32654372+jackmorrison12@users.noreply.github.com>
2020-05-01 23:52:49 +0000

    Deploying to gh-pages from  @ 545a7179e821383aa24745b8521390104d01427e 🚀

---
+++ precache-manifest.914f9c3a38932292443f2deaaaa66a38.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a69f1794736ff124f4eba2c81915998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7acb4280a2a9c89cde2e",
+    "url": "/static/css/main.78bb0efa.chunk.css"
+  },
+  {
+    "revision": "dcabd6170f421cac8350",
+    "url": "/static/js/3.8bb412d9.chunk.js"
+  },
+  {
+    "revision": "b2de105c8cddf672f1d2366ae6b89aa8",
+    "url": "/static/js/3.8bb412d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82bf0fd03fd6d2f8597",
+    "url": "/static/js/4.8956462d.chunk.js"
+  },
+  {
+    "revision": "e4d85a7242366d583d3c",
+    "url": "/static/js/5.6bde807d.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.6bde807d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f8c51e85e93d1d29e3",
+    "url": "/static/js/6.2667ed46.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.2667ed46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7acb4280a2a9c89cde2e",
+    "url": "/static/js/main.90d49537.chunk.js"
+  },
+  {
+    "revision": "08dc79a4fc3ba73c89bb",
+    "url": "/static/js/runtime-main.20702c1b.js"
+  },
+  {
+    "revision": "25165955c949c4dab002",
+    "url": "/static/js/xlsx.f329d66e.chunk.js"
+  },
+  {
+    "revision": "69cfc62a22b6dd2f54c77f88e81aa3f8",
+    "url": "/static/media/404.69cfc62a.png"
+  },
+  {
+    "revision": "fe509d05eb596a9d276c7e96900c894a",
+    "url": "/static/media/arm11.fe509d05.jpg"
+  },
+  {
+    "revision": "99fe96e84f5655a3054bfb686806a461",
+    "url": "/static/media/avatar.99fe96e8.jpg"
+  },
+  {
+    "revision": "909afa688a54ee6bb01c65547f32b206",
+    "url": "/static/media/bmb-1.909afa68.png"
+  },
+  {
+    "revision": "904203b6c7c2c453a4ab481b4683f5fc",
+    "url": "/static/media/bmb-2.904203b6.png"
+  },
+  {
+    "revision": "5a0426913ca962487560144f233feb45",
+    "url": "/static/media/bmb-3.5a042691.png"
+  },
+  {
+    "revision": "3a812d8d01fd445fd73bdcf13b4d8d04",
+    "url": "/static/media/bmb-4.3a812d8d.png"
+  },
+  {
+    "revision": "c7f6c302095c9b4e1d115ac5018440f4",
+    "url": "/static/media/burst-my-bubble.c7f6c302.jpg"
+  },
+  {
+    "revision": "bc9919409c47cd5c222a060b77ec128b",
+    "url": "/static/media/canvas.bc991940.jpg"
+  },
+  {
+    "revision": "98374d13d4ddd95625f54b5f0aaafe6c",
+    "url": "/static/media/cas-1.98374d13.jpg"
+  },
+  {
+    "revision": "a12b51941010a08aa25604ee6e2114ac",
+    "url": "/static/media/cas-2.a12b5194.jpg"
+  },
+  {
+    "revision": "1f5e10a6d57b518f145162108e676db9",
+    "url": "/static/media/cas-3.1f5e10a6.jpg"
+  },
+  {
+    "revision": "17c8997bc2e05cdf7fd41c5cd826d67d",
+    "url": "/static/media/cas-4.17c8997b.jpg"
+  },
+  {
+    "revision": "2ac49db521934ca77333ee380580d025",
+    "url": "/static/media/cas-5.2ac49db5.png"
+  },
+  {
+    "revision": "43f4a43cdebe6cabc206256e877399dc",
+    "url": "/static/media/correct-a-spine.43f4a43c.jpg"
+  },
+  {
+    "revision": "a7b3e1296e5b94a17b2fb21dca669f14",
+    "url": "/static/media/devpost.a7b3e129.png"
+  },
+  {
+    "revision": "c9b2d5fb350215182c54a066a065636a",
+    "url": "/static/media/epq.c9b2d5fb.jpg"
+  },
+  {
+    "revision": "69de13f3f899be232ae7420833fd7717",
+    "url": "/static/media/imaths.69de13f3.jpg"
+  },
+  {
+    "revision": "186be69739fdce7f559f7af685131436",
+    "url": "/static/media/mentor-me.186be697.jpg"
+  },
+  {
+    "revision": "9090ddac8665d82c92c61671c9b4cc7d",
+    "url": "/static/media/monzo-receipts.9090ddac.jpg"
+  },
+  {
+    "revision": "df79141ff45b7c256ba99921644e844a",
+    "url": "/static/media/monzo-receipts.df79141f.png"
+  },
+  {
+    "revision": "1a54482049a1756af714aa963e63e468",
+    "url": "/static/media/musictech.1a544820.jpg"
+  },
+  {
+    "revision": "0117980469a412f95c5ebc8c5ca32598",
+    "url": "/static/media/pintos.01179804.jpg"
+  },
+  {
+    "revision": "97585185e866228fcddf78759d0fc2c7",
+    "url": "/static/media/pyplay-1.97585185.jpg"
+  },
+  {
+    "revision": "c88fadde9910cc3e8581d8297212ff21",
+    "url": "/static/media/pyplay.c88fadde.jpg"
+  },
+  {
+    "revision": "6e6d5041dacc53c9f0e823fc00f19093",
+    "url": "/static/media/python.6e6d5041.jpg"
+  },
+  {
+    "revision": "c2eb2a46995c3c3d5942a267013973ee",
+    "url": "/static/media/snake.c2eb2a46.jpg"
+  },
+  {
+    "revision": "b6189844dd469e7c2d2f23174844a200",
+    "url": "/static/media/topics.b6189844.jpg"
+  },
+  {
+    "revision": "30253c279faba2e77120862dd54d49d4",
+    "url": "/static/media/wacc.30253c27.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.f508dcfb23432c7aaeacc1e25a7302c8.js"
+  "/precache-manifest.914f9c3a38932292443f2deaaaa66a38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
